0e7224c1a032712add800a01fcdf25b49fb97223
leo10099 <leandrohanc@gmail.com>
2022-11-19 10:17:48 -0300

    New try for fixing memory leak

--- workers/src/portales/scraper.ts
+++ workers/src/portales/scraper.ts
@@ -14,177 +14,180 @@ import {
 
 export const getPortalsMostRead = async (
 	itemLimit: number
-): Promise<PortalsData> => {
+): Promise<PortalsData | undefined> => {
 	/* Scraping start */
 	const browser = await chromium.launch({
 		headless: true,
 		chromiumSandbox: false,
 	});
-	const elDestape = await browser.newPage();
-	await elDestape.goto(PORTAL_EL_DESTAPE_URL);
 
-	await elDestape.waitForTimeout(8000);
+	try {
+		const elDestape = await browser.newPage();
+		await elDestape.goto(PORTAL_EL_DESTAPE_URL);
 
-	/* El destape */
+		await elDestape.waitForTimeout(8000);
 
-	const elDestapeArticleTitles = await (
-		await elDestape.locator(".mas_vistas .titulo h2").allInnerTexts()
-	).slice(0, itemLimit);
+		/* El destape */
+		const elDestapeArticleTitles = await (
+			await elDestape.locator(".mas_vistas .titulo h2").allInnerTexts()
+		).slice(0, itemLimit);
 
-	const elDestapelinkLocator = await elDestape.locator(
-		".mas_vistas .titulo h2 a"
-	);
-	const elDestapeArticleLinks = await elDestapelinkLocator.evaluateAll(
-		(list, { itemLimit, PORTAL_EL_DESTAPE_URL }) => {
-			return list
-				.map(
-					linkElement =>
-						`${PORTAL_EL_DESTAPE_URL}${linkElement.getAttribute("href")}`
-				)
-				.slice(0, itemLimit);
-		},
-		{ PORTAL_EL_DESTAPE_URL, itemLimit }
-	);
+		const elDestapelinkLocator = await elDestape.locator(
+			".mas_vistas .titulo h2 a"
+		);
+		const elDestapeArticleLinks = await elDestapelinkLocator.evaluateAll(
+			(list, { itemLimit, PORTAL_EL_DESTAPE_URL }) => {
+				return list
+					.map(
+						linkElement =>
+							`${PORTAL_EL_DESTAPE_URL}${linkElement.getAttribute("href")}`
+					)
+					.slice(0, itemLimit);
+			},
+			{ PORTAL_EL_DESTAPE_URL, itemLimit }
+		);
 
-	/* Telam */
+		/* Telam */
+		const telam = await browser.newPage();
+		await telam.goto(PORTAL_TELAM_URL);
 
-	const telam = await browser.newPage();
-	await telam.goto(PORTAL_TELAM_URL);
+		await telam.waitForTimeout(3000);
+		await telam.mouse.wheel(0, 6000);
+		await telam.waitForTimeout(3000);
 
-	await telam.waitForTimeout(3000);
-	await telam.mouse.wheel(0, 6000);
-	await telam.waitForTimeout(3000);
+		const telamArticleTitles = await (
+			await telam.locator(".moreread h5").allInnerTexts()
+		).slice(0, itemLimit);
 
-	const telamArticleTitles = await (
-		await telam.locator(".moreread h5").allInnerTexts()
-	).slice(0, itemLimit);
+		const telamLinkLocator = await telam.locator(".moreread h5 a");
 
-	const telamLinkLocator = await telam.locator(".moreread h5 a");
+		const telamArticleLinks = await telamLinkLocator.evaluateAll(
+			(list, { itemLimit, PORTAL_TELAM_URL }) => {
+				return list
+					.map(
+						linkElement =>
+							`${PORTAL_TELAM_URL}${linkElement.getAttribute("href")}`
+					)
+					.slice(0, itemLimit);
+			},
+			{ PORTAL_TELAM_URL, itemLimit }
+		);
 
-	const telamArticleLinks = await telamLinkLocator.evaluateAll(
-		(list, { itemLimit, PORTAL_TELAM_URL }) => {
-			return list
-				.map(
-					linkElement =>
-						`${PORTAL_TELAM_URL}${linkElement.getAttribute("href")}`
-				)
-				.slice(0, itemLimit);
-		},
-		{ PORTAL_TELAM_URL, itemLimit }
-	);
+		await telam.close();
 
-	await telam.close();
+		/* Infobae */
+		const infobae = await browser.newPage();
+		await infobae.goto(PORTAL_INFOBAE_URL);
 
-	/* Infobae */
+		await infobae.waitForTimeout(3000);
 
-	const infobae = await browser.newPage();
-	await infobae.goto(PORTAL_INFOBAE_URL);
+		const infobaeArticleTitles = await (
+			await infobae
+				.locator(".most-read-container .most-read-item .most-read-headline")
+				.allInnerTexts()
+		).slice(0, itemLimit);
 
-	await infobae.waitForTimeout(3000);
+		const infobaeLinkLocator = await infobae.locator(
+			".most-read-container .most-read-item .headline-link"
+		);
 
-	const infobaeArticleTitles = await (
-		await infobae
-			.locator(".most-read-container .most-read-item .most-read-headline")
-			.allInnerTexts()
-	).slice(0, itemLimit);
+		const infobaeArticleLinks = await infobaeLinkLocator.evaluateAll(
+			(list, { itemLimit, PORTAL_INFOBAE_URL }) => {
+				return list
+					.map(
+						linkElement =>
+							`${PORTAL_INFOBAE_URL}${linkElement.getAttribute("href")}`
+					)
+					.slice(0, itemLimit);
+			},
+			{ PORTAL_INFOBAE_URL, itemLimit }
+		);
 
-	const infobaeLinkLocator = await infobae.locator(
-		".most-read-container .most-read-item .headline-link"
-	);
+		await infobae.close();
 
-	const infobaeArticleLinks = await infobaeLinkLocator.evaluateAll(
-		(list, { itemLimit, PORTAL_INFOBAE_URL }) => {
-			return list
-				.map(
-					linkElement =>
-						`${PORTAL_INFOBAE_URL}${linkElement.getAttribute("href")}`
-				)
-				.slice(0, itemLimit);
-		},
-		{ PORTAL_INFOBAE_URL, itemLimit }
-	);
+		/* Clarín */
+		const clarin = await browser.newPage();
+		await clarin.goto(PORTAL_CLARIN_URL);
 
-	await infobae.close();
+		await clarin.waitForTimeout(3000);
 
-	/* Clarín */
+		const clarinArticleTitles = await (
+			await clarin.locator(".The__most__seen .box-container h2").allInnerTexts()
+		).slice(0, itemLimit);
 
-	const clarin = await browser.newPage();
-	await clarin.goto(PORTAL_CLARIN_URL);
+		const clarinLinkLocator = await clarin.locator(
+			".The__most__seen article a"
+		);
 
-	await clarin.waitForTimeout(3000);
+		const clarinArticleLinks = await clarinLinkLocator.evaluateAll(
+			(list, { itemLimit, PORTAL_CLARIN_URL }) => {
+				return list
+					.map(
+						linkElement =>
+							`${PORTAL_CLARIN_URL}${linkElement.getAttribute("href")}`
+					)
+					.slice(0, itemLimit);
+			},
+			{ PORTAL_CLARIN_URL, itemLimit }
+		);
 
-	const clarinArticleTitles = await (
-		await clarin.locator(".The__most__seen .box-container h2").allInnerTexts()
-	).slice(0, itemLimit);
+		await clarin.close();
 
-	const clarinLinkLocator = await clarin.locator(".The__most__seen article a");
+		/* La Nación */
+		const laNacion = await browser.newPage();
+		await laNacion.goto(PORTAL_LA_NACION_URL);
 
-	const clarinArticleLinks = await clarinLinkLocator.evaluateAll(
-		(list, { itemLimit, PORTAL_CLARIN_URL }) => {
-			return list
-				.map(
-					linkElement =>
-						`${PORTAL_CLARIN_URL}${linkElement.getAttribute("href")}`
-				)
-				.slice(0, itemLimit);
-		},
-		{ PORTAL_CLARIN_URL, itemLimit }
-	);
+		await laNacion.waitForTimeout(3000);
 
-	await clarin.close();
+		const laNacionArticleTitles = await (
+			await laNacion
+				.locator('[data-module="tema_ranking"] article h2')
+				.allInnerTexts()
+		).slice(0, itemLimit);
 
-	/* La Nación */
+		const laNacionLinkLocator = await laNacion.locator(
+			'[data-module="tema_ranking"] article section.mod-description a'
+		);
 
-	const laNacion = await browser.newPage();
-	await laNacion.goto(PORTAL_LA_NACION_URL);
+		const laNacionArticleLinks = await laNacionLinkLocator.evaluateAll(
+			(list, { itemLimit, PORTAL_LA_NACION_URL }) => {
+				return list
+					.map(
+						linkElement =>
+							`${PORTAL_LA_NACION_URL}${linkElement.getAttribute("href")}`
+					)
+					.slice(0, itemLimit);
+			},
+			{ PORTAL_LA_NACION_URL, itemLimit }
+		);
 
-	await laNacion.waitForTimeout(3000);
+		await laNacion.close();
 
-	const laNacionArticleTitles = await (
-		await laNacion
-			.locator('[data-module="tema_ranking"] article h2')
-			.allInnerTexts()
-	).slice(0, itemLimit);
-
-	const laNacionLinkLocator = await laNacion.locator(
-		'[data-module="tema_ranking"] article section.mod-description a'
-	);
-
-	const laNacionArticleLinks = await laNacionLinkLocator.evaluateAll(
-		(list, { itemLimit, PORTAL_LA_NACION_URL }) => {
-			return list
-				.map(
-					linkElement =>
-						`${PORTAL_LA_NACION_URL}${linkElement.getAttribute("href")}`
-				)
-				.slice(0, itemLimit);
-		},
-		{ PORTAL_LA_NACION_URL, itemLimit }
-	);
-
-	await laNacion.close();
-	await browser.close();
-
-	return {
-		elDestape: {
-			articles: elDestapeArticleTitles,
-			links: elDestapeArticleLinks,
-		},
-		telam: {
-			articles: telamArticleTitles,
-			links: telamArticleLinks,
-		},
-		infobae: {
-			articles: infobaeArticleTitles,
-			links: infobaeArticleLinks,
-		},
-		clarin: {
-			articles: clarinArticleTitles,
-			links: clarinArticleLinks,
-		},
-		laNacion: {
-			articles: laNacionArticleTitles,
-			links: laNacionArticleLinks,
-		},
-	};
+		return {
+			elDestape: {
+				articles: elDestapeArticleTitles,
+				links: elDestapeArticleLinks,
+			},
+			telam: {
+				articles: telamArticleTitles,
+				links: telamArticleLinks,
+			},
+			infobae: {
+				articles: infobaeArticleTitles,
+				links: infobaeArticleLinks,
+			},
+			clarin: {
+				articles: clarinArticleTitles,
+				links: clarinArticleLinks,
+			},
+			laNacion: {
+				articles: laNacionArticleTitles,
+				links: laNacionArticleLinks,
+			},
+		};
+	} catch (e) {
+		console.log(e);
+	} finally {
+		await browser.close();
+	}
 };
