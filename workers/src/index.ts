205d1a55e136fb92086be97207b543a653d349ed
Leandro Hanc <leandrohanc@gmail.com>
2023-03-16 11:52:21 -0300

    Debug twitter articles

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -165,7 +165,6 @@ async function portalsMostReadArticles(db: Db) {
 
 		if (DATABASE_CONNECTION_URI && topArticles) {
 			const date = new Date();
-			console.log({ topArticles });
 			await persistPortalsData(topArticles, date, db);
 		}
 	} catch (error) {
