dd7978a47a0fcc7968d67f6e3e173ba9d84e22f6
Leandro Hanc <leandrohanc@gmail.com>
2023-04-05 14:54:37 -0300

    chore(workers): cambiar scraping the spotify a viernes

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -187,11 +187,11 @@ async function bootstrap() {
 	/* Get portals top articles cron - At 44 every hour */
 	schedule("44 * * * *", () => portalsMostReadArticles(db));
 
-	/* Get Spotify top songs and artists cron - At 3:30 every Wednesay */
-	schedule("30 3 * * Wed", () => spotifyTopSongsAndArtistsScraper(db));
+	/* Get Spotify top songs and artists cron - At 3:30 every Friday */
+	schedule("30 3 * * Fri", () => spotifyTopSongsAndArtistsScraper(db));
 
-	/* Get Spotify top pocasts cron - At 3:50 every Wednesday */
-	schedule("50 3 * * Wed", () => spotifyTopPodcasts(db));
+	/* Get Spotify top pocasts cron - At 3:50 every Friday */
+	schedule("50 3 * * Fri", () => spotifyTopPodcasts(db));
 
 	console.log("Worker started succesfully, waiting for jobs...");
 }
